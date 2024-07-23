6a5af2293ae43ebf68e63ade5d2c5618cba4d1a3
nhathao19 <141889402+nhathao19@users.noreply.github.com>
2024-07-23 21:28:42 +0700

    inital commit

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignUpForm } from './components/SignUpForm'
+
+const SignUpPage = () => {
+    return (
+        <div className='flex h-svh items-center'><SignUpForm /></div>
+    )
+}
+
+export default SignUpPage
